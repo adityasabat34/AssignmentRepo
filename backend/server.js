166bb668dba0a22bbcfb90e72c686a8e07b5fde8
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-02-22 13:14:41 +0530

    Environment and MongoDB set up done.

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,12 @@
 import express from 'express';
 import products from './data/products.js';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -11,6 +18,10 @@ app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
-app.listen(5000, () => {
-  console.log('Server running on port 5000 ');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgMagenta
+  );
 });
