699fdeb95fe599513e0597fd757d065cea8d4774
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-02-23 17:56:04 +0530

    All products , single product reducer and action done.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,15 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import { thunk } from 'redux-thunk';
+import {
+  porductListReducer,
+  productDetailsReducer,
+} from './reducers/productReducer';
 
-const reducer = combineReducers({});
+const reducer = combineReducers({
+  producList: porductListReducer,
+  productDetails: productDetailsReducer,
+});
 
 const initialState = {};
 
