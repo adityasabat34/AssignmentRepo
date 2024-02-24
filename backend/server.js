be1f4bc9405831c6b0e85b24d0a4636d9037a01c
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2024-02-24 19:26:29 +0530

    READY FOR PRODUCTION DEPLOYMENT

--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,30 @@ import colors from 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import { notFound, errorHandler } from './Error/ErrorHandler.js';
+import path from 'path';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
-
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+app.use(express.json());
 
 app.use('/api/products', productRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running!');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
